031d885b6ec31a9f0e23cc63602c2b0315813feb
Linhmon999 <90033974+Linhmon999@users.noreply.github.com>
2021-09-20 16:49:12 +0700

    edit lần 12

---
+++ wp-admin/upload.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://linhmon999.github.io/shop_with_us/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fupload.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://linhmon999.github.io/shop_with_us/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fupload.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://linhmon999.github.io/shop_with_us/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fupload.php&reauth=1">https://linhmon999.github.io/shop_with_us/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fupload.php&reauth=1</a></p>
+	</body>
+</html>
